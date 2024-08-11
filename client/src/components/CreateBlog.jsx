978635474294d5af30af5c6ff1a570a7af3d7da6
karuppasamyGB <148546076+karuppasamyGB@users.noreply.github.com>
2024-08-11 17:18:48 +0530

    Update CreateBlog.jsx

--- client/src/components/CreateBlog.jsx
+++ client/src/components/CreateBlog.jsx
@@ -30,7 +30,7 @@ const CreateBlog = () => {
     };
 
     try {
-      await axios.post('https://blog-app-4ddw.onrender.com/api/blog/create', newPost);
+      await axios.post('http://localhost:3500/api/blog/create', newPost);
       navigate('/');
     } catch (error) {
       console.error('There was an error creating the blog post!', error);

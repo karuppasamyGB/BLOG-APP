638545b7eae1aa478adcf4f5aeaea0bd3dfc5b98
karuppasamyGB <148546076+karuppasamyGB@users.noreply.github.com>
2024-08-11 13:00:46 +0530

    Update SecretToken.js

--- server/util/SecretToken.js
+++ server/util/SecretToken.js
@@ -1,11 +1,3 @@
-// require("dotenv").config();
-// const jwt = require("jsonwebtoken");
-
-// module.exports.createSecretToken = (id) => {
-//   return jwt.sign({ id }, process.env.TOKEN_KEY, {
-//     expiresIn: 3 * 24 * 60 * 60,
-//   });
-// };
 const jwt = require('jsonwebtoken');
 
 const createSecretToken = (userId) => {
